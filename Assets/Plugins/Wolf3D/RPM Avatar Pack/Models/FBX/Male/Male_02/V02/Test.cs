17dd10f2ca8982a9211c37658b2f6b445a6a4826
Steven_Oh <ysoh1205@gmail.com>
2021-05-11 22:09:55 +0900

    First Commit

---
+++ Assets/Plugins/Wolf3D/RPM Avatar Pack/Models/FBX/Male/Male_02/V02/Test.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+    protected Rigidbody rb;
+    protected float moveSpeed = 0.1f;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+            transform.Translate(Vector3.forward * moveSpeed);
+        if (Input.GetKey(KeyCode.DownArrow))
+            transform.Translate(Vector3.back * moveSpeed);
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Translate(Vector3.left * moveSpeed);
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Translate(Vector3.right * moveSpeed);
+        if (Input.GetKey(KeyCode.U))
+            transform.Translate(Vector3.up * moveSpeed);
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(Vector3.down * moveSpeed);
+
+    }
+}
